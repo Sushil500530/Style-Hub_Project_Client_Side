581f6d57e51dcb90b530e6f8c87a65714feb3412
Sushil97 <sushil500530@gmail.com>
2023-12-13 19:12:24 +0600

    add firebase connection and other

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './routes/Router.jsx'
 import { Toaster } from 'react-hot-toast'
+import AuthProvider from './provider/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Router}>
-      <Toaster />
-    </RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={Router}>
+        <Toaster />
+      </RouterProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, updateProfile } from "firebase/auth";
+import auth from './firebase.config';
+
+export const AuthContext = createContext(null);
+const googleProvider = new GoogleAuthProvider();
+
+
+const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] = useState(true);
+
+
+
+    const createUser = (email,password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    const updateUserProfile = (name,photo) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, {
+            displayName:name, photoURL:photo
+        })
+    }
+
+    const loginUser = (email,password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth,googleProvider)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth,currentUser => {
+            setUser(currentUser);
+            console.log(currentUser.email);
+            // if(currentUser){
+            //     const userInfo = {email: currentUser.email}
+            // }
+        })
+        return () => {
+            unsubscribe ();
+        }
+    },[])
+
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        loginUser,
+        googleSignIn,
+        updateUserProfile,
+
+
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
